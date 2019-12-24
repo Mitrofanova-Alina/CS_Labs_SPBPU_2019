1715fabbe7b51da46d49cd8d92583c17e8dd0462
Alina Mitrofanova <alina010299@mail.ru>
2019-12-24 18:45:55 +0300

    Initial Lab2 commit

---
+++ Lab2_Section_Body_Rotation/venv/Scripts/easy_install-3.7-script.py
@@ -0,0 +1,12 @@
+#!D:\alina\Documents\GitHub\CS_Labs_SPBPU_2019\Lab2_Section_Body_Rotation\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install-3.7')()
+    )
